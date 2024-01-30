9a1aacfead34a37b90265b1b49502d901a749338
Julio Jimenez <julio@julioj.com>
2024-01-30 11:12:16 -0500

    racket
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -14,21 +14,18 @@ const FeatureList = [
   },
   {
     title: "Focus on What Matters",
-    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
+    Svg: require("@site/static/img/information.svg").default,
     description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
+      <>Get the information you need, when you need it. No more, no less.</>
     ),
   },
   {
-    title: "Powered by React",
-    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
+    title: "Powered by Racket",
+    Svg: require("@site/static/img/racket-logo.svg").default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        We love Racket, and welcome all Racketeers to make contributions to
+        sbom.cc.
       </>
     ),
   },
