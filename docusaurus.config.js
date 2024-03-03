fad8ec00f5e344e63a83571850c34f3e47aeb0ad
Julio Jimenez <julio@julioj.com>
2024-03-03 05:09:49 -0500

    remove blog
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,7 +76,7 @@ const config = {
             position: "left",
             label: "Docs",
           },
-          { to: "/blog", label: "Blog", position: "left" },
+          // { to: "/blog", label: "Blog", position: "left" },
           {
             href: "https://github.com/sbomcc/sbomcc",
             label: "GitHub",
@@ -116,10 +116,10 @@ const config = {
           {
             title: "More",
             items: [
-              {
-                label: "Blog",
-                to: "/blog",
-              },
+              // {
+              //   label: "Blog",
+              //   to: "/blog",
+              // },
               {
                 label: "GitHub",
                 href: "https://github.com/sbomcc/sbomcc",
